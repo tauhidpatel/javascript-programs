ffa7d07235bfedfa72196a8762a797cd97d1aa87
tauhid patel <tauhidpatel18@gmail.com>
2022-06-08 20:46:06 +0530

    adding linear search program

---
+++ linearSearch.js
@@ -0,0 +1,12 @@
+function linearSearch(arr) {
+    let i = 2 ;
+    for(let num of arr) {
+        if(i == 2) {
+            return true;
+        }
+            return false;
+    }
+}
+
+const res = linearSearch([1, 2, 3, 6, 8]);
+console.log(res);
