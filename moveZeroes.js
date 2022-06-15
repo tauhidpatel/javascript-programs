e1d609c161885e85ff557fdd6bd22a26ec8e814d
tauhid patel <tauhidpatel18@gmail.com>
2022-06-15 16:40:27 +0530

    adding moveZeroes program

---
+++ moveZeroes.js
@@ -0,0 +1,21 @@
+var moveZeroes = function(nums) {
+  
+    let index = 0;
+
+    for(let i = 0; i<nums.length; i++) {
+        const num = nums[i];
+
+        if(num !== 0) {
+            nums[index] = num;
+            index++;
+            // console.log(nums);
+        }
+    }
+
+    for(let i = index; i<nums.length; i++) {
+        nums[i] = 0;
+        console.log(nums);
+    }
+};
+
+moveZeroes([0,1,0,3,12])
