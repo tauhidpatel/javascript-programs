64386949e32f25e8a7cfc70c6e1989c3fd88e607
tauhid patel <tauhidpatel18@gmail.com>
2022-07-04 20:23:55 +0530

    adding max sub array program

---
+++ maximumSubArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    let maxCurrent = nums[0];
+    let maxGlobal = nums[0];
+
+    for(let i=1; i<nums.length; i++) {
+        maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+        
+        if(maxCurrent > maxGlobal) {
+            maxGlobal = maxCurrent;
+        }
+    }
+
+    return maxGlobal
+};
