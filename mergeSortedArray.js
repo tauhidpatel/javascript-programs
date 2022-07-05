2beee6bedb8945ed9fbea2763821580b9f00cb50
tauhid patel <tauhidpatel18@gmail.com>
2022-07-05 14:58:30 +0530

    adding merge sort array

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,7 +1,23 @@
-const arr1 = [1, 4, 7, 8, 9];
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    let first = m - 1;
+    let second = n - 1;
 
-const arr2 = [2, 3, 6];
+    for(let i = m + n - 1; i>=0; i--) {
+        if(second < 0) break;
 
-const arr3 = arr1.concat(arr2);
-
-console.log(arr3.sort().flat(() => {},depth=1));
+        if(nums1[first] > nums2[second]) {
+            nums1[i] = nums1[first];
+            first--;
+        } else {
+            nums1[i] = nums2[second];
+            second--;
+        }
+    }
+};
