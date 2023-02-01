6ea643a856d008a812d02bb56fc92848f9445d1b
Tauhid Patel <tauhidpatel18@gmail.com>
2023-02-01 20:16:03 +0530

    adding fizzbuzz program

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let result = [];
+    
+    for(let i=1; i<=n; i++) {
+        if(i%15 === 0) {
+            result.push("FizzBuzz");
+        } else if (i%3 === 0) {
+            result.push("Fizz");
+        } else if (i%5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return (result);
+};
+
+let res = fizzBuzz(0);
+console.log(res);
