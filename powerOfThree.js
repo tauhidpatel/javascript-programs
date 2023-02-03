14215a5ac537d9a20631a8ee93bdaa0bb471418e
Tauhid Patel <tauhidpatel18@gmail.com>
2023-02-03 20:36:10 +0530

    adding powerOfThree program

---
+++ powerOfThree.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isPowerOfThree = function(n) {
+    
+    if( n === 0 ) {
+        return false;
+    }
+    
+    while(n % 3 === 0) {
+        n /= 3;
+    }
+    
+    return n === 1;
+    
+};
+
+let res = isPowerOfThree(9);
+console.log(res);
